6fd98a822bd556dc0f4a2c18852b6f6e1ae25b70
chaehs <chaehschaehs@gmail.com>
2022-09-02 15:16:12 +0900

    Intent 추가

--- app/src/main/java/org/techtown/test02/MainActivity.kt
+++ app/src/main/java/org/techtown/test02/MainActivity.kt
@@ -1,11 +1,21 @@
 package org.techtown.test02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val moveToPageBtn = findViewById<Button>(R.id.moveToPageBtn)
+
+        moveToPageBtn.setOnClickListener{
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
